59700f1627b79214e3c986877769414ce23b3bf7
Benjamin <btleffler@gmail.com>
2013-09-10 10:19:32 -0400

    Fix up routes to help with missing files and path normalization

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,46 @@
-
-/*
- * GET home page.
- */
-
-var fs = require("fs");
+var fs = require("fs"),
+    path = require("path");
 
 function renderDirectory (req, res, stats) {
-  res.render("directory", {
-    "title": req.path,
-    "files": []
-  });
+    fs.readdir(req.path, function (err, files) {
+        if (err)
+            fileNotFound(res);
+
+        res.render("directory", {
+            "title": req.path,
+            "files": files.unshift("..") // Allow for navigation back
+        });
+    });
 }
 
 function renderFile (req, res, stats) {
-  // figure out mime-type, file name, file size
-  // Force download
+    // figure out mime-type, file name, file size
+    // Force download
+}
+
+function fileNotFound(res) {
+    res.status(404);
 }
 
 exports.file = function fileRoute (req, res){
-  fs.stat(req.path, function stats(err, stats) {
-    if (stats.isDirectory())
-      return renderDirectory(req, res, stats);
+    var uri = path.normalize(req.path);
     
-    return renderFile(req, res, stats);
-  });
+    if (uri !== req.path)
+        return res.redirect(uri);
+
+    fs.exists(req.path, function(exists) {
+        if (!exists)
+            return fileNotFound(res);
+
+        fs.stat(req.path, function stats(err, stats) {
+            if (err)
+                return fileNotFound(res);
+
+            if (stats.isDirectory())
+                return renderDirectory(req, res, stats);
+
+            return renderFile(req, res, stats);
+        });
+    });
 };
+
