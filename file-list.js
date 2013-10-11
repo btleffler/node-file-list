e795c6746ac98c8bf874ace2a2caac130c908bf4
btleffler <btleffler@gmail.com>
2013-10-11 09:26:17 -0400

    Get rid of some of the things express generates by default that we don't need. https://github.com/btleffler/node-file-list/issues/1

--- file-list.js
+++ file-list.js
@@ -14,10 +14,8 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
