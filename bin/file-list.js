f46419422b017fdc98b75d9ac24192001473ee46
Benjamin <btleffler@gmail.com>
2013-11-02 09:34:11 -0400

    Add port option to serve

--- bin/file-list.js
+++ bin/file-list.js
@@ -6,16 +6,18 @@ var program = require("commander"),
     pathSepExp = require("../lib/FileCollector.js").terminatingPathSep,
     fileList = require("../lib");
 
-program.version("0.1.1");
+program.version("0.2.0");
 
 program
     .command("serve")
     .description("Serve a directory")
     .option("-r, --root [directory]", "root directory to be served", path.normalize)
+    .option("-p, --port [port]", "port to listen to", parseInt)
     .action(function (options) {
-        var root = options.root || process.cwd();
+        var root = options.root || process.cwd(),
+            port = options.port || 3000;
         root = root.replace(pathSepExp, '');
-        fileList.cli.startServer(root);
+        fileList.cli.startServer(root, port);
     });
 
 program.parse(process.argv);
