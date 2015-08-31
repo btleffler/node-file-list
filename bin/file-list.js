8b30a10ef9d443234dd0ed92bcb7ba566949486c
Ben <btleffler@gmail.com>
2015-08-30 21:29:35 -0400

    More fixes  - Don't show hidden directories when showHidden is false (Unix only)  - When root is set to `.`, use `process.cwd()` instead. `.` thows    things off with hidden files at the root level on Unix

--- bin/file-list.js
+++ bin/file-list.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 
 var program = require("commander"),
-    fs = require("fs"),
-    path = require("path"),
-    pathSepExp = require("../lib/FileCollector.js").terminatingPathSep,
-    fileList = require("../lib");
+	fs = require("fs"),
+	path = require("path"),
+	pathSepExp = require("../lib/FileCollector.js").terminatingPathSep,
+	fileList = require("../lib");
 
 program.version("0.2.0");
 
 program
-    .command("serve")
-    .description("Serve a directory")
-    .option("-r, --root [directory]", "root directory to be served", path.normalize)
-    .option("-p, --port [port]", "port to listen to", parseInt)
-    .option("--showHidden", "show hidden files (unix only)")
-    .action(function (options) {
-        var root = options.root || process.cwd(),
-            port = options.port || 3000,
-            showHidden = options.showHidden || false;
+	.command("serve")
+  	.description("Serve a directory")
+  	.option("-r, --root [directory]", "root directory to be served", path.normalize)
+  	.option("-p, --port [port]", "port to listen to", parseInt)
+  	.option("--showHidden", "show hidden files (unix only)")
+  	.action(function (options) {
+			var root = options.root || process.cwd(),
+				port = options.port || 3000,
+				showHidden = options.showHidden || false;
 
-        root = root.replace(pathSepExp, '');
-        fileList.cli.startServer(root, port, showHidden);
-    });
+			root = root.replace(pathSepExp, '');
+			fileList.cli.startServer(root, port, showHidden);
+		});
 
 program.parse(process.argv);
